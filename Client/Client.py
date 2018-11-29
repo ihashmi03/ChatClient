ab0e9910567ef6a96ce7650f7fadfc8f02a98b9c
ihashmi03 <ihashmi03@gmail.com>
2018-11-28 21:39:39 -0500

    ChatClientV2
    Updated to allow messages between server and client

--- Client/Client.py
+++ Client/Client.py
@@ -1,4 +1,6 @@
 import socket
+import select
+import sys
 
 HOST = '127.0.0.1'
 PORT = 65432
@@ -6,7 +8,20 @@ PORT = 65432
 s = socket.socket()
 s.connect((HOST,PORT))
 print('Connected to Server')
-s.sendall(b'Hello World!')
-data = s.recv(1024)
 
-print('Received', repr(data))
+while True:
+    sockets_list = [sys.stdin, s]
+
+    read_sockets,write_socket,error_socket = select.select(sockets_list,[],[])
+
+    for socks in read_sockets:
+        if socks == s:
+            message = socks.recv(1024)
+            print message
+        else:
+            message = sys.stdin.readline()
+            s.send(message)
+            sys.stdout.write("<You>")
+            sys.stdout.write(message)
+            sys.stdout.flush()
+s.close()
--- Server/Server.py
+++ Server/Server.py
@@ -1,26 +1,73 @@
 #!/usr/bin/env python3
-
 import socket
+import select
+import sys
+from thread import *
 
-HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+HOST = '127.0.0.1'  # localhost
+PORT = 65432        # Port 
 
 try:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print 'Socket Created Successfully'
+    
     s.bind((HOST, PORT))
-    s.listen(5)
-    conn, addr = s.accept()
-    #with conn:
-    print('Connected by', addr)
+    
+    s.listen(25)
+    
+    list_of_clients = []
+
+    def groupthread(conn, addr):
+        #send a greeting
+        conn.send('Welcome to this chat')
+
+        while True:
+            try:
+                data = conn.recv(1024)
+                if data:
+                    print "<" + addr[0] + "> " + data
+                    #print data 
+                    message = sys.stdin.readline() 
+                    conn.send('<Server> '+message) 
+                    sys.stdout.write("<You>") 
+                    sys.stdout.write(message) 
+                    sys.stdout.flush()
+                    message_to_send = "<" + addr[0] + "> " + data 
+                    broadcast(message_to_send, conn)
+
+                else:
+
+                    remove(conn)
+                
+            except:
+                continue
+
+    #broadcast function
+    def broadcast(message, connection):
+        for clients in list_of_clients:
+            if clients!=connection:
+                try:
+                    clients.send(message)
+                except:
+                    clients.close()
+                    remove(clients)
+
+    #function to remove clients
+    def remove(connection):
+        if connection in list_of_clients:
+            list_of_clients.remove(connection)
+
     while True:
-        data = conn.recv(1024)
-        if not data:
-            break
-        conn.sendall(data)
-        #conn.close()
+
+        conn, addr = s.accept()
+        list_of_clients.append(conn)
+
+        print addr[0] + ' connected'
+
+        start_new_thread(groupthread,(conn,addr))
+        
 except socket.error as err:
     print 'socket creation failed with error %s' %(err)
-finally:
-    if conn:
-        conn.close()
+
+conn.close()
+s.close()
